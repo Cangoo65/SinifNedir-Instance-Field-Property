abd02add3384a6c47bcb2180cebe1df8d2e02424
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-17 21:12:14 +0300

    Proje dosyası ekle.

---
+++ Calisan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinifNedir_Instance_Field_Property
+{
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı :" +  Ad);
+            Console.WriteLine("Çalışanın Soyadı :" +  Soyad);
+            Console.WriteLine("Çalışanın Numarası :" + No);
+            Console.WriteLine("çalışanın Departmanı :" + Departman);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using SinifNedir_Instance_Field_Property;
+
+Calisan calisan1 = new();
+calisan1.Ad = "Ali";
+calisan1.Soyad = "Gür";
+calisan1.No = 5367825;
+calisan1.Departman = "Spor";
+calisan1.CalisanBilgileri();
+
+Calisan calisan2 = new();
+calisan2.Ad = "Funda";
+calisan2.Soyad = "Sürer";
+calisan2.No = 875434;
+calisan2.Departman = "Satın Alma";
+calisan2 .CalisanBilgileri();
+
+Console.Read();
